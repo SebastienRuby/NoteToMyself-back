ea5cdc176c17fe28a0c3a507f60ac992498c74d0
VALLAUD-Ulrich <ulrichvallaud@hotmail.fr>
2022-10-04 15:54:23 +0200

    test 3.7.1

--- app/router/router.js
+++ app/router/router.js
@@ -34,25 +34,25 @@ router.delete('/memento', authMiddleware.checkToken, controllerMemento.deleteMem
 
 
 
-app.post('/upload', function(req, res) {
-  let sampleFile;
-  let uploadPath;
+// app.post('/upload', function(req, res) {
+//   let sampleFile;
+//   let uploadPath;
 
-  if (!req.files || Object.keys(req.files).length === 0) {
-    return res.status(400).send('No files were uploaded.');
-  }
+//   if (!req.files || Object.keys(req.files).length === 0) {
+//     return res.status(400).send('No files were uploaded.');
+//   }
 
-  // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
-  sampleFile = req.files.sampleFile;
-  uploadPath = __dirname + '/somewhere/on/your/server/' + sampleFile.name;
+//   // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
+//   sampleFile = req.files.sampleFile;
+//   uploadPath = __dirname + '/somewhere/on/your/server/' + sampleFile.name;
 
-  // Use the mv() method to place the file somewhere on your server
-  sampleFile.mv(uploadPath, function(err) {
-    if (err)
-      return res.status(500).send(err);
+//   // Use the mv() method to place the file somewhere on your server
+//   sampleFile.mv(uploadPath, function(err) {
+//     if (err)
+//       return res.status(500).send(err);
 
-    res.send('File uploaded!');
-  });
-});
+//     res.send('File uploaded!');
+//   });
+// });
  
-module.exports = router;
+// module.exports = router;
